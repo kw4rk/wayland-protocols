4f0516bed3c4134c3d73c2d784c5f5307ffd93e5
Daniel Stone <daniels@collabora.com>
2023-04-04 11:29:16 +0200

    ci: bump Meson to v0.58.0
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- .gitlab-ci/debian-install.sh
+++ .gitlab-ci/debian-install.sh
@@ -2,6 +2,8 @@
 
 # Note: don't forget to bump FDO_DISTRIBUTION_TAG when editing this file!
 
+pip3 install --user git+https://github.com/mesonbuild/meson.git@0.58.0
+
 git clone --branch 1.20.0 --depth=1 https://gitlab.freedesktop.org/wayland/wayland
 cd wayland/
 git show -s HEAD
